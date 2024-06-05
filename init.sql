5b58aece489b65ad92afb4cbab788633bdf88b98
Ali Shah <89487837+Alishah634@users.noreply.github.com>
2024-06-04 21:47:04 -0400

    Fixed duplicate user additions in init.sql

--- init.sql
+++ init.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS openid (
     id INT PRIMARY KEY
 );
 
-INSERT INTO users (username, email, password) VALUES 
+INSERT IGNORE INTO users (username, email, password) VALUES 
 ('Ancient One', 'ancientone@gmail.com', 'pw1'),
 ('Alice', 'alice@example.com', 'pw2'),
 ('Bob', 'bob@example.com', 'pw3'),
