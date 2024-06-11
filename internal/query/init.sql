4c8e5a80288f378fc9792b1f15d570a10f6d5bf5
seoyoungcho213 <cho411@purdue.edu>
2024-06-11 15:01:15 -0400

    Reorganized database & Added timestamp to every table

--- internal/query/init.sql
+++ internal/query/init.sql
@@ -1,34 +1,46 @@
 -- Copyright (c) 2024 Seoyoung Cho, Ali A. Shah, Carlos Andres Cotera Jurado.
 CREATE TABLE IF NOT EXISTS users
 (
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email    VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created  timestamp DEFAULT NOW()
+    id         BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS openid
 (
-    id INT PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS accounts
-(
-    id   INT PRIMARY KEY,
-    bank VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS transactions
-(
-    id       INT PRIMARY KEY,
-    category VARCHAR(255) NOT NULL,
-    amount   INT          NOT NULL,
-    bank     VARCHAR(255) NOT NULL
+    id         BIGINT(20) UNSIGNED NOT NULL PRIMARY KEY,
+    created_at timestamp DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS assets
 (
-    id   INT PRIMARY KEY,
-    type VARCHAR(255) NOT NULL
+    id       INT UNSIGNED PRIMARY KEY,
+    asset_id INT UNSIGNED,
+    user_id  BIGINT(20) UNSIGNED,
+    balance  BIGINT(20) UNSIGNED
+);
+
+CREATE TABLE IF NOT EXISTS liquid
+(
+    id   INT UNSIGNED PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS solid
+(
+    id         INT UNSIGNED PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS transactions
+(
+    id         INT UNSIGNED PRIMARY KEY,
+    category   VARCHAR(255) NOT NULL,
+    amount     INT          NOT NULL,
+    bank       VARCHAR(255) NOT NULL,
+    created_at timestamp DEFAULT NOW()
 );
