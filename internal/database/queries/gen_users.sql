0caa5e3444497cc30ac079e6939669166b57bf63
seoyoungcho213 <cho411@purdue.edu>
2024-07-05 23:58:53 -0400

    Removed gen_users.sql due to hashedPW and our unit test. No longer useful

--- internal/database/queries/gen_users.sql
+++
@@ -1,6 +0,0 @@
--- TODO(@seoyoungcho213) : modify this based on AddUser()
-INSERT INTO users (username, email, password)
-VALUES ('Ancient One', 'ancientone@gmail.com', 'pw1'),
-       ('Alice', 'alice@example.com', 'pw2'),
-       ('Bob', 'bob@example.com', 'pw3'),
-       ('Charlie', 'charlie@example.com', 'pw4');
